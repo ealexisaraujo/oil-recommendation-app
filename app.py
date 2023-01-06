db01ff3a45f9a30d527c53a7d7144300a476ecfb
Alexis <ala1289@gmail.com>
2023-01-06 14:23:31 -0600

    fix

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
-from pathlib import Path
 import streamlit as st
 import pandas as pd
+import os
+from dotenv import load_dotenv
 from PIL import Image
+from utils import GetLatLon2,distance_estac,transform_df_map,marker_rest
 
 # Tools
 
 import folium
-from shapely.geometry import Polygon
-import numpy as np
-import random
-import time
-from pyproj import Geod
-
-from shapely import wkt
-from geopandas import datasets, GeoDataFrame, read_file, points_from_xy
-
-from folium.plugins import MeasureControl
-from folium.plugins import MarkerCluster
-
-from utils import GetLatLon2,cal_dist,distance_estac,transform_df_map,marker_rest
+from geopandas import GeoDataFrame, points_from_xy
 from streamlit_folium import folium_static
-
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 image = Image.open('images_streamlit/1_Fuel-prices.jpg')
@@ -35,8 +22,7 @@ if app_mode == 'Run App':
   st.title('Nearby Oil Station App')
   st.markdown('App Description')
 
-  file_path = Path('DF_STATIONS.CSV').parents[0] / 'DF_STATIONS.CSV'
-  df_map = pd.read_csv(file_path)
+  df_map = pd.read_csv('DF_STATIONS.csv')
   cities =  list(df_map['Municipio'].unique())
 
   # Crear columnas usar st.columns especificando el ancho de las columnas
